33bbf0110508b97fc08271d475fb7cabb9135d5c
Peter Lee <petershuion@gmail.com>
2022-11-06 18:35:32 +0800

    Add GetMultipleJockeyTest

--- JCDataExtractor/JCDataExtractor.Services/WebDataService.cs
+++ JCDataExtractor/JCDataExtractor.Services/WebDataService.cs
@@ -9,106 +9,12 @@ namespace JCDataExtractor.Services
         public WebDataService()
         {
         }
+
         /*
             1. https://racing.hkjc.com/racing/information/chinese/Racing/RaceCard.aspx?RaceDate=2022/11/06&Racecourse=ST&RaceNo=1
-
             呢條係排位表既Link，所有基本資料都係呢個排位表道黎。
             其實條Link 上面有一D變數，日期、場地、同埋場次
             其餘既就全部唔會變
-
-            Target:
-            table: racecardlist
-            header tr: trBg01 tdAlignC boldFont13
-                        <tr class="trBg01 tdAlignC boldFont13">
-                            <td width="40px" style="width: 40px;">馬匹編號</td>
-                            <td width="80px" style="width: 80px;">6次近績</td>
-                            <td width="40px" style="width: 40px;">綵衣</td>
-                            <td width="100px" style="width: 100px;">馬名</td>
-                            <td width="40px" style="width: 40px; display: none;">烙號</td>
-                            <td width="40px" style="width: 40px;">負磅</td>
-                            <td width="135px" style="width: 135px;">騎師</td>
-                            <td width="40px" style="width: 40px; display: none;">可能超磅</td>
-                            <td width="40px" style="width: 40px;">檔位</td>
-                            <td width="100px" style="width: 100px;">練馬師</td>
-                            <td width="40px" style="width: 40px; display: none;">國際評分</td>
-                            <td width="40px" style="width: 40px;">評分</td>
-                            <td width="50px" style="width: 50px;">評分+/-</td>
-                            <td width="40px" style="width: 40px;">排位體重</td>
-                            <td width="50px" style="width: 50px; display: none;">排位體重+/-</td>
-                            <td width="40px" style="width: 40px; display: none;">最佳時間</td>
-                            <td width="40px" style="width: 40px; display: none;">馬齡</td>
-                            <td width="40px" style="width: 40px; display: none;">分齡讓磅</td>
-                            <td width="40px" style="width: 40px; display: none;">性別</td>
-                            <td width="40px" style="width: 40px; display: none;">今季獎金</td>
-                            <td width="50px" style="width: 50px;">優先參賽次序</td>
-
-                            <td width="40px" style="width: 40px;">配備</td>
-                            <td width="220px" style="width: 220px; display: none;">馬主</td>
-                            <td width="40px" style="width: 40px; display: none;">父系</td>
-                            <td width="40px" style="width: 40px; display: none;">母系</td>
-                            <td width="40px" style="width: 40px; display: none;">進口類別</td>
-                        </tr>
-
-                        <tr class="trBg01 tdAlignC boldFont13">
-                            <td width="40px" style="width: 40px;">Horse No.</td>
-                            <td width="80px" style="width: 80px;">Last 6 Runs</td>
-                            <td width="40px" style="width: 40px;">Colour</td>
-                            <td width="100px" style="width: 100px;">Horse</td>
-                            <td width="40px" style="width: 40px; display: none;">Brand No.</td>
-                            <td width="40px" style="width: 40px;">Wt.</td>
-                            <td width="135px" style="width: 135px;">Jockey</td>
-                            <td width="40px" style="width: 40px; display: none;">Over Wt.</td>
-                            <td width="40px" style="width: 40px;">Draw</td>
-                            <td width="100px" style="width: 100px;">Trainer</td>
-                            <td width="40px" style="width: 40px; display: none;">Int'l Rtg.</td>
-                            <td width="40px" style="width: 40px;">Rtg.</td>
-                            <td width="50px" style="width: 50px;">Rtg.+/-</td>
-                            <td width="40px" style="width: 40px;">Horse Wt. (Declaration)</td>
-                            <td width="50px" style="width: 50px; display: none;">Wt.+/- (vs Declaration)</td>
-                            <td width="40px" style="width: 40px; display: none;">Best Time</td>
-                            <td width="40px" style="width: 40px; display: none;">Age</td>
-                            <td width="40px" style="width: 40px; display: none;">WFA</td>
-                            <td width="40px" style="width: 40px; display: none;">Sex</td>
-                            <td width="40px" style="width: 40px; display: none;">Season Stakes</td>
-                            <td width="50px" style="width: 50px;">Priority</td>
-
-                            <td width="40px" style="width: 40px;">Gear</td><td width="220px" style="width: 220px; display: none;">Owner</td>
-                            <td width="40px" style="width: 40px; display: none;">Sire</td>
-                            <td width="40px" style="width: 40px; display: none;">Dam</td>
-                            <td width="40px" style="width: 40px; display: none;">Import Cat.</td>
-                        </tr>
-
-            row html: f_tac f_fs13
-                        <tr class="f_tac f_fs13">
-                            <td style="width: 40px;">1</td>
-                            <td style="width: 80px;">14/11/12/11/8/14</td>
-                            <td style="width: 40px;"><img src="/racing/content/Images/RaceColor/G232.gif" alt=""></td>
-                            <td style="width: 100px;"><a href="/racing/information/chinese/Horse/Horse.aspx?HorseId=HK_2021_G232" onclick="window.open(this.href, '_blank', 'status=no,toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=812,height=600'); return false;">美麗新星</a></td>
-                            <td style="width: 40px; display: none;">G232</td>
-                            <td style="width: 40px;">135</td>
-                            <td style="width: 135px;">
-                                    <a href="/racing/information/chinese/Jockey/JockeyProfile.aspx?JockeyId=BV" onclick="window.open(this.href, '_blank', 'status=no,toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=812,height=600'); return false;">波健士</a>
-                            </td>
-                            <td style="width: 40px; display: none;"></td>
-                            <td style="width: 40px;">5</td>
-                            <td style="width: 100px;"><a href="/racing/information/chinese/Trainers/TrainerProfile.aspx?TrainerId=SJJ" onclick="window.open(this.href, '_blank', 'status=no,toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=yes,scrollbars=yes,left=20,top=20,width=812,height=600'); return false;">蔡約翰</a></td>
-                            <td style="width: 40px; display: none;">-</td>
-                            <td style="width: 40px;">40</td>
-                            <td style="width: 50px;">-</td>
-                            <td style="width: 40px;">1052</td>
-                            <td style="width: 50px; display: none;">+1</td>
-                            <td style="width: 40px; display: none;"></td>
-                            <td style="width: 40px; display: none;">4</td>
-                            <td style="width: 40px; display: none;">-</td>
-                            <td style="width: 40px; display: none;">閹</td>
-                            <td style="width: 40px; display: none;">0</td>
-                            <td style="width: 50px;">* 1</td>
-
-                            <td style="width: 40px;">H1/TT</td><td style="width: 220px; display: none;">郭少明</td>
-                            <td style="width: 40px; display: none;">Territories</td>
-                            <td style="width: 40px; display: none;">Folly Bridge</td>
-                            <td style="width: 40px; display: none;">PP</td>
-                        </tr>
          */
         public static async Task<List<RaceCardEntry>> GetRaceCardEntries(DateTime raceDate, string raceCourse, int raceNo)
         {
@@ -140,14 +46,10 @@ namespace JCDataExtractor.Services
                             Height = 768
                         });
 
-                        //#racecardlist > tbody > tr > td > table > tbody > tr:nth-child(1)
-                        //#main > div > div.card > div.card_body > table > tbody > tr:nth-child(1) > td:nth-child(5) > span
-                        ///document.querySelector("#main > div > div.card > div.card_body > table > tbody > tr:nth-child(1) > td:nth-child(10) > span")
-
                         Thread.Sleep(1000);
 
                         //map html table data to list of object via js querySelector here
-                        var jsShortTable = @"() => {
+                        var jsRaceCard = @"() => {
                         const selectors = Array.from(document.querySelectorAll('#racecardlist > tbody > tr > td > table > tbody > tr '));
                         return selectors.map( (tr) => { 
                             const tds = Array.from(tr.querySelectorAll('td'));
@@ -181,11 +83,14 @@ namespace JCDataExtractor.Services
                             });
                         }";
 
-                        var raceCardResults = await page.EvaluateFunctionAsync<RaceCardEntry[]>(jsShortTable);
+                        var raceCardResults = await page.EvaluateFunctionAsync<RaceCardEntry[]>(jsRaceCard);
 
                         if (raceCardResults != null && raceCardResults.Length != 0)
                         {
                             result = raceCardResults.ToList();
+                        }
+                        else {
+                            throw new Exception("No race card result found!");
                         }
 
                         return result;
@@ -203,50 +108,8 @@ namespace JCDataExtractor.Services
 
         /*
             2. https://racing.hkjc.com/racing/information/Chinese/racing/Draw.aspx
-
-            呢一條係當日賽事所有場次既檔位統計，基本上係一頁Show晒咁多場，所以條Link 無變數。
-            table: #innerContent > div.Draw.commContent > div:nth-child(5)
-            Info: #innerContent > div.Draw.commContent > table > tbody > tr > td.f_fs13.font_wb <- 06/11/2022 沙田
-            table header: #race1 
-                          #race2 > td <- 第 2 場 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 1200米 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 草地 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; "C+3" 賽道
-
-                #innerContent > div.Draw.commContent > div.racingNum.top_races.js_racecard_rt_num > table > tbody > tr > td:nth-child(11)  <- count how many race here
-                
-                #innerContent > div.Draw.commContent > div:nth-child(5) <- table 1
-                #innerContent > div.Draw.commContent > div:nth-child(7) <- table 2
-                #innerContent > div.Draw.commContent > div:nth-child(9) <- table 3 ...
-                
-            row:#innerContent > div.Draw.commContent > div:nth-child(5) > table > tbody > tr:nth-child(1)
-                #innerContent > div.Draw.commContent > div:nth-child(5) > table > tbody > tr:nth-child(1) > td.f_pr.scale > span.win > img .width
-                #innerContent > div.Draw.commContent > div:nth-child(5) > table > tbody > tr:nth-child(1) > td.f_pr.scale > span.placed > img .width (100% = 360px)
-                
-                #innerContent > div.Draw.commContent > div:nth-child(23) > table > tfoot > tr > td:nth-child(2) > span:nth-child(1) <- hot, total 3 items
-
-            row html:
-                    檔位
-                    出賽次數
-                    冠
-                    亞
-                    季
-                    殿
-                    勝出率%
-                    入Q率%
-                    上名率%
-                    前4名率%
-
-                    Draw
-                    Runners
-                    Win
-                    2nd
-                    3rd
-                    4th
-                    W%
-                    Q%
-                    P%
-                    F%
-                            
+            呢一條係當日賽事所有場次既檔位統計，基本上係一頁Show晒咁多場，所以條Link 無變數。            
          */
-
         /// <summary>
         /// Get the list of Draw Statistics for the coming race
         /// </summary>
@@ -283,8 +146,7 @@ namespace JCDataExtractor.Services
 
                         Thread.Sleep(1000);
 
-                        //check how many race on the page
-                        //#innerContent > div.Draw.commContent > div.racingNum.top_races.js_racecard_rt_num > table > tbody > tr > td <- count td
+                        //check how many race on the page                        
                         var jsRaceCount = @"() => {
                         const selectors = Array.from(document.querySelectorAll('#innerContent > div.Draw.commContent > div.racingNum.top_races.js_racecard_rt_num > table > tbody > tr > td '));
                         return selectors.length;}";
@@ -361,42 +223,11 @@ namespace JCDataExtractor.Services
 
         /*
          3. https://racing.hkjc.com/racing/information/Chinese/Jockey/JockeyRanking.aspx?Season=Current&View=Numbers&Racecourse=ALL
-
             https://racing.hkjc.com/racing/information/Chinese/Jockey/JockeyRanking.aspx?Season=Previous&View=Numbers&Racecourse=ALL
-
             呢一條係騎師統計表，當中有變數，Current代表今季資料，Previous代表上季
-
-                <td width="160px">騎師</td>
-                <td width="70px">冠</td>
-                <td width="70px">亞</td>
-                <td width="70px">季</td>
-                <td width="70px">殿</td>
-                <td width="70px">第五</td>
-                <td width="120px">總出賽次數</td>
-                <td>所贏獎金</td>
-
-                row: #innerContent > div.commContent > div.Ranking > div:nth-child(3) > table > tbody:nth-child(2) > tr:nth-child(1) <-first
-                     #innerContent > div.commContent > div.Ranking > div:nth-child(3) > table > tbody:nth-child(2) > tr:nth-child(25) <-last
-
-                row html:
-                    <tr>
-                        <td class="f_fs14 f_tal">
-                            <a href="/racing/information/Chinese/Jockey/JockeyPastRec.aspx?JockeyId=PZ&amp;Season=Current">
-                                潘頓
-                            </a>
-                        </td>
-                        <td>32</td>
-                        <td>18</td>
-                        <td>9</td>
-                        <td>17</td>
-                        <td>10</td>
-                        <td>133</td>
-                        <td class="f_tar">$42,497,125</td>
-                    </tr>
          */
         public static async Task<List<JockeyRaceRow>> GetJockeyRankingTable(string seasonType)
         {
-            //https://racing.hkjc.com/racing/information/Chinese/Jockey/JockeyRanking.aspx?Season=Current&View=Numbers&Racecourse=ALL
             string url = string.Format(@"https://racing.hkjc.com/racing/information/Chinese/Jockey/JockeyRanking.aspx?Season=" + seasonType + @"&View=Numbers&Racecourse=ALL");
             var result = new List<JockeyRaceRow>();
 
@@ -426,14 +257,13 @@ namespace JCDataExtractor.Services
 
                         Thread.Sleep(1000);
 
-                        //check how many race on the page
-                        //#innerContent > div.Draw.commContent > div.racingNum.top_races.js_racecard_rt_num > table > tbody > tr > td <- count td
+                        //check how many race on the page                        
                         var jsJockeyRank = @"() => {
                         const selectors = Array.from(document.querySelectorAll('#innerContent > div.commContent > div.Ranking > div:nth-child(3) > table > tbody:nth-child(2) > tr '));
                         return selectors.map( (tr) => { 
                                     const tds = Array.from(tr.querySelectorAll('td'));
                                     return { 
-                                             jockeyName: tds[0].querySelector('a').innerHTML,
+                                             jockeyName: tds[0].querySelector('a').innerHTML.replaceAll('\n','').replace(/ /g,''),
                                              jockeyId:   tds[0].querySelector('a').href.split('=')[1].split('&')[0],
                                              count1st:   tds[1].innerHTML,
                                              count2nd:   tds[2].innerHTML,
@@ -441,7 +271,7 @@ namespace JCDataExtractor.Services
                                              count4th:   tds[4].innerHTML,
                                              count5th:   tds[5].innerHTML,
                                              totalRide:  tds[6].innerHTML,
-                                             stakesWon:  tds[7].innerHTML.replace('$','').replace(',','')
+                                             stakesWon:  tds[7].innerHTML.replace('$','').replaceAll(',','')
                                             }
                                         });
                                 }";
@@ -463,8 +293,7 @@ namespace JCDataExtractor.Services
 
         /*
             4. https://racing.hkjc.com/racing/information/Chinese/Trainers/TrainerRanking.aspx?Season=Current&View=Numbers&Racecourse=ALL
-
-            https://racing.hkjc.com/racing/information/Chinese/Trainers/TrainerRanking.aspx?Season=Previous&View=Numbers&Racecourse=ALL
+               https://racing.hkjc.com/racing/information/Chinese/Trainers/TrainerRanking.aspx?Season=Previous&View=Numbers&Racecourse=ALL
 
             呢一條係練馬師統計表，同騎師統計表一樣，當中有一個變數，Current代表今季資料，Previous代表上季
          */
@@ -485,9 +314,6 @@ namespace JCDataExtractor.Services
             JockeyId同馬匹一樣，可以係排位表個超連結上面提取到
             最麻煩就係PageNum，1代表第一頁，如果得2頁既話，你輸入3佢都會比繼續比第二頁你，所以我個程式要檢查有無「下一頁」呢個字去判斷有無下一頁。    
          */
-
-        //#innerContent > div.jockeyPastRec.commContent > div.ridingRec > table > tbody > tr:nth-child(2) > td:nth-child(1) <- count if td == 13
-        //#innerContent > div.jockeyPastRec.commContent > div.ridingRec > table > tbody > tr:nth-child(3)        
         public static async Task<List<RidingRecord>> GetRidingRecords(string jockeyID, string seasonType, int pageID = 1)
         {
             //https://racing.hkjc.com/racing/information/Chinese/Jockey/JockeyPastRec.aspx?JockeyId=BV&Season=Current&PageNum=1
@@ -523,7 +349,7 @@ namespace JCDataExtractor.Services
                         });
 
                         Thread.Sleep(1000);
-                        //#innerContent > div.jockeyPastRec.commContent > div.ridingRec > table > tbody > tr:nth-child(1)
+
                         var jsRidingRecords = @"() => {
                         const selectors = Array.from(document.querySelectorAll('#innerContent > div.jockeyPastRec.commContent > div.ridingRec > table > tbody > tr '));
                         return selectors.map( (tr) => { 
@@ -550,7 +376,15 @@ namespace JCDataExtractor.Services
                                 }";
 
                         var ridingRecords = await page.EvaluateFunctionAsync<RidingRecord[]>(jsRidingRecords);
-                        result = ridingRecords.Where(r => r != null).ToList();
+                        if (ridingRecords != null && ridingRecords.Length != 0)
+                        {
+                            result = ridingRecords.Where(r => r != null).ToList();
+                        }
+                        else
+                        {
+                            throw new Exception("No riding records found!");
+                        }
+                        
                         return result;
                     }
                 }
--- JCDataExtractor/JCDataExtractor.ServicesTests/WebDataServiceTests.cs
+++ JCDataExtractor/JCDataExtractor.ServicesTests/WebDataServiceTests.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Polly;
 
 namespace JCDataExtractor.Services.Tests
 {
@@ -15,7 +16,11 @@ namespace JCDataExtractor.Services.Tests
         [TestMethod()]
         public async Task GetRaceCardEntriesTest()
         {
-            var results = await WebDataService.GetRaceCardEntries(DateTime.Parse("2022/11/06"), "st", 1);
+            var polly = Policy
+              .Handle<Exception>()
+              .RetryAsync(3, (exception, retryCount, context) => Console.WriteLine($"try: {retryCount}, Exception: {exception.Message}"));
+
+            var results = await polly.ExecuteAsync(async () => await WebDataService.GetRaceCardEntries(DateTime.Parse("2022/11/06"), "st", 1));
             Assert.AreEqual(true, (results.Count != 0 ? true : false));
         }
 
@@ -28,16 +33,82 @@ namespace JCDataExtractor.Services.Tests
 
         [TestMethod()]
         public async Task GetRidingRecordsTest()
-        {
-            var results = await WebDataService.GetRidingRecords("PZ", "Current");
+        {            
+            var polly = Policy
+            .Handle<Exception>()
+            .RetryAsync(3, (exception, retryCount, context) => Console.WriteLine($"try: {retryCount}, Exception: {exception.Message}"));
+
+            var results = await polly.ExecuteAsync(async () => await WebDataService.GetRidingRecords("PZ", "Current"));
+
             Assert.AreEqual(true, (results.Count != 0 ? true : false));
         }
 
         [TestMethod()]
         public async Task GetJockeyRankingTableTest()
         {
-            var results = await WebDataService.GetJockeyRankingTable("Current");
+            var seasonType = "Current";
+            var polly = Policy
+           .Handle<Exception>()
+           .RetryAsync(3, (exception, retryCount, context) => Console.WriteLine($"try: {retryCount}, Exception: {exception.Message}"));
+
+            var results = await polly.ExecuteAsync(async () => await WebDataService.GetJockeyRankingTable(seasonType));
+
             Assert.AreEqual(true, results.Count != 0 ? true : false);
+        }
+
+        [TestMethod()]
+        public async Task GetMultipleJockeyTest()
+        {
+            var seasonType = "Current";
+            var JockeyList = new List<Jockey>();
+
+            var polly = Policy
+           .Handle<Exception>()
+           .RetryAsync(3, (exception, retryCount, context) => Console.WriteLine($"try: {retryCount}, Exception: {exception.Message}"));
+
+            var jockeyRanks = await polly.ExecuteAsync(async () => await WebDataService.GetJockeyRankingTable(seasonType));
+
+            //foreach (var jRank in jockeyRanks)
+            for(int i = 0; i < 3; i++) //test only top 3 here
+            {
+                var jRank = jockeyRanks[i];
+                var jockey = new Jockey();
+                jockey.id = jRank.jockeyId;
+                jockey.name = jRank.jockeyName;
+
+                var fullRidingRecords = new List<RidingRecord>();
+                var pageCount = 1;
+
+                //Check if jockey has ride before
+                if (jRank.totalRide != 0) {
+                    do
+                    {
+                        var ridingResults = await polly.ExecuteAsync(async () => await WebDataService.GetRidingRecords(jRank.jockeyId, seasonType, pageCount));
+                        fullRidingRecords.AddRange(ridingResults);
+                        pageCount++;
+                    }
+                    while (fullRidingRecords.OrderBy(r => r.index).FirstOrDefault().index > 1);
+                }
+
+                //Map ranking to yearly race stats
+                var jStats = new JockeyRaceStats();
+                jStats.year = (seasonType == "Current" ? DateTime.Now.Year : DateTime.Now.Year - 1);
+                jStats.count1st = jRank.count1st;
+                jStats.count2nd = jRank.count2nd;
+                jStats.count3rd = jRank.count3rd;
+                jStats.count4th = jRank.count4th;
+                jStats.count5th = jRank.count5th;
+                jStats.totalRide = jRank.totalRide;
+                jStats.stakesWon = jRank.stakesWon;
+
+                jockey.JockeyRaceStatsList = new List<JockeyRaceStats>();
+                jockey.JockeyRaceStatsList.Add(jStats);
+                jockey.RidingRecords = fullRidingRecords;
+
+                JockeyList.Add(jockey);
+            }
+
+            Assert.AreEqual(true, JockeyList.Count != 0 ? true : false);
         }
     }
 }
